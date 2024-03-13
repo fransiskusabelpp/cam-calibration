2d7e5fe59659180a65be8ddb46485dcbcd52de89
Fransiskus Abel <110248358+fransiskusabelpp@users.noreply.github.com>
2024-03-13 10:26:30 +0700

    Add files via upload

---
+++ create-chessboard.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+
+# Dimensi papan catur
+n_rows = 7  # Number of rows
+n_cols = 10  # Number of columns
+
+# Ukuran kotak
+square_size = 50
+
+# Warna kotak
+white = (255, 255, 255)
+black = (0, 0, 0)
+
+# Buat gambar kosong
+img = np.zeros((n_rows * square_size, n_cols * square_size, 3), np.uint8)
+
+# Isi kotak dengan warna
+for i in range(n_rows):
+    for j in range(n_cols):
+        color = white if (i + j) % 2 == 0 else black
+        cv2.rectangle(img, (j * square_size, i * square_size),
+                      ((j + 1) * square_size, (i + 1) * square_size), color, -1)
+
+# Tampilkan gambar
+cv2.imshow('Chessboard', img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Simpan gambar (opsional)
+cv2.imwrite('chessboard_custom.png', img)
+
